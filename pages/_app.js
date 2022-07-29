bc9b0f9c5516aa47a5094524e93c449afe826359
narapat hiranjaratsang <top.narapat@gmail.com>
2022-07-29 10:42:10 +0700

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+
+import { Navbar, Footer } from '../components';
+import '../styles/globals.css';
+
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+
+    <Script src="https://kit.fontawesome.com/0a9f559ac4.js" crossorigin="anonymous" />
+  </ThemeProvider>
+);
+
+export default MyApp;
